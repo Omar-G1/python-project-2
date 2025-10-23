fe3ec5d406f2b169f95f7ca50b6e2dc70dcf431c
Omar-G1 <omar.haribe@protonmail.com>
2025-10-23 23:32:55 +0300

    Refactor task functions to use user database
    Updated task management functions to integrate with user database and handle task uniqueness.

--- tasks.py
+++ tasks.py
@@ -1,18 +1,23 @@
-tasks = {}
-user_tasks = {}
-
-def task_name_exists(name):
-    return any(task['name'] == name for task in tasks.values())
-
+import users  
 
 def add_task(username, name, description):
-    if username not in user_tasks:
-        user_tasks[username] = []
-    user_tasks[username].append({"name": name, "description": description})
+
+    if username not in users.users_db:
+        print("User not found.")
+        return False
+    
+    user_tasks = users.users_db[username].setdefault("tasks", {})
+    if any(t["name"] == name for t in user_tasks.values()):
+        print("Task name already exists for this user.")
+        return False
+    
+    task_id = max(user_tasks.keys(), default=0) + 1
+    user_tasks[task_id] = {"name": name, "description": description}
+   
+    users.save_users(users.users_db)
+    return True
 
 def view_tasks(username):
-    return user_tasks.get(username, [])
-
-
-def view_tasks(username):
-    return user_tasks.get(username, [])
+    if username not in users.users_db:
+        return {}
+    return users.users_db[username].get("tasks", {})
