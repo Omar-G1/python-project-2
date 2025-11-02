86f4e2cffe93a479752a1ee05b3d57c52976018b
Omar-G1 <omar.haribe@protonmail.com>
2025-11-02 16:20:23 +0200

    Reformat models.py with consistent imports and structure

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
- from sqlalchemy import create_engine, Column, Integer, String, Text, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, Text, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 import bcrypt
